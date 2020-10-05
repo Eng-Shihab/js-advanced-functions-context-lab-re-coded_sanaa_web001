c34eaf02ebcd9b63f3d9f4f7db3e29acca949f8d
Shihab AL-Mekhlafi <shihabalmekhlafy@gmail.com>
2020-10-05 19:16:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,82 +1,84 @@
-let createEmployeeRecord = function(row){
-    return {
-        firstName: row[0],
-        familyName: row[1],
-        title: row[2],
-        payPerHour: row[3],
-        timeInEvents: [],
-        timeOutEvents: []
+/* Your Code Here */
+
+/*
+ We're giving you this function. Take a look at it, you might see some usage
+ that's new and different. That's because we're avoiding a well-known, but
+ sneaky bug that we'll cover in the next few lessons!
+ As a result, the lessons for this function will pass *and* it will be available
+ for you to use if you need it!
+ */
+
+function createEmployeeRecord (arraySt){
+
+  return {
+      firstName : arraySt[0],
+      familyName: arraySt[1],
+      title: arraySt[2],
+      payPerHour: arraySt[3],
+      timeInEvents : [],
+      timeOutEvents:[]
+  }
+}
+
+
+function createEmployeeRecords (arraySt){
+  return arraySt.map (x => createEmployeeRecord(x));
+}
+
+function createTimeInEvent (date){
+  let arrDa = date.split(" ") ;
+  let ob = {
+    type:"TimeIn" ,
+    hour: parseInt(arrDa[1]),
+    date:arrDa[0]
+  };
+  this.timeInEvents.push(ob);
+  return this;
+}
+
+function createTimeOutEvent ( date){
+  let arrDa = date.split(" ") ;
+  let ob = {
+    type:"TimeOut" ,
+    hour: parseInt(arrDa[1]),
+    date:arrDa[0]
+  };
+  this.timeOutEvents.push(ob);
+  return this;
+}
+
+function hoursWorkedOnDate (date){
+  let arrDa = date.split(" ") ;
+  for (let i = 0 ; i < this.timeInEvents.length ;i++ ) {
+    if(arrDa[0] === this.timeInEvents[i].date){
+      return (this.timeOutEvents[i].hour - this.timeInEvents[i].hour )/ 100;
     }
+  }
 }
 
-let createEmployeeRecords = function(employeeRowData) {
-    return employeeRowData.map(function(row){
-        return createEmployeeRecord(row)
-    })
+function wagesEarnedOnDate (date){
+  return hoursWorkedOnDate.call(this,date) * this.payPerHour ;
 }
 
-let createTimeInEvent = function(employee, dateStamp){
-    let [date, hour] = dateStamp.split(' ')
-
-    employee.timeInEvents.push({
-        type: "TimeIn",
-        hour: parseInt(hour, 10),
-        date,
-    })
-
-    return employee
+function allWagesFor (){
+  let hour = 0;
+  for (let i = 0 ; i < this.timeInEvents.length ; i++ ) {
+    hour += wagesEarnedOnDate.call(this ,this.timeInEvents[i].date);
+  }
+  return hour  ;
 }
 
-let createTimeOutEvent = function(employee, dateStamp){
-    let [date, hour] = dateStamp.split(' ')
-
-    employee.timeOutEvents.push({
-        type: "TimeOut",
-        hour: parseInt(hour, 10),
-        date,
-    })
-
-    return employee
+function findEmployeeByFirstName (srcArray,firstName ){
+  let s= srcArray.find((x) => x.firstName === firstName);
+  return s;
 }
 
-let hoursWorkedOnDate = function(employee, soughtDate){
-    let inEvent = employee.timeInEvents.find(function(e){
-        return e.date === soughtDate
-    })
-
-    let outEvent = employee.timeOutEvents.find(function(e){
-        return e.date === soughtDate
-    })
-
-    return (outEvent.hour - inEvent.hour) / 100
-}
-
-let wagesEarnedOnDate = function(employee, dateSought){
-    let rawWage = hoursWorkedOnDate(employee, dateSought)
-        * employee.payPerHour
-    return parseFloat(rawWage.toString())
-}
-
-let allWagesFor = function(employee){
-    let eligibleDates = employee.timeInEvents.map(function(e){
-        return e.date
-    })
-
-    let payable = eligibleDates.reduce(function(memo, d){
-        return memo + wagesEarnedOnDate(employee, d)
-    }, 0)
-
-    return payable
-}
-
-let findEmployeeByFirstName = function(srcArray, firstName) {
-  return srcArray.find(function(rec){
-    return rec.firstName === firstName
-  })
-}
-
-let calculatePayroll = function(arrayOfEmployeeRecords){
-    return arrayOfEmployeeRecords.reduce(function(memo, rec){
-        return memo + allWagesFor(rec)
-    }, 0)
+function calculatePayroll (srcArray ){
+  let sum = 0 ;
+  for (let i = 0 ; i < srcArray.length ; i++ ) {
+    for (let j = 0 ; j < srcArray[i].timeInEvents.length ; j++ ){
+      sum += wagesEarnedOnDate.call(srcArray[i],srcArray[i].timeInEvents[j].date) ;
+    }
+  }
+  return sum ;
 }
